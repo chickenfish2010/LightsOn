e68a29737e8893d629687b11d41c0290ce7d14c0
Carver Petty <cpetty1@harding.edu>
2015-04-21 14:14:29 -0500

    Added buttons and flip functionality.

--- Lights On/ViewController.swift
+++ Lights On/ViewController.swift
@@ -9,7 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var Lights: [UIButton]!
+    @IBAction func LightFlipped(sender: UIButton) {
+        if(sender == Lights[0])
+        {
+            LightSwitch(Lights[0]);
+            LightSwitch(Lights[1]);
+            LightSwitch(Lights[3]);
+            LightSwitch(Lights[4]);
+        }
+        else if(sender == Lights[1])
+        {
+            LightSwitch(Lights[0]);
+            LightSwitch(Lights[1]);
+            LightSwitch(Lights[2]);
+            LightSwitch(Lights[3]);
+            LightSwitch(Lights[4]);
+            LightSwitch(Lights[5]);
+        }
+        else if(sender == Lights[2])
+        {
+            LightSwitch(Lights[1]);
+            LightSwitch(Lights[2]);
+            LightSwitch(Lights[4]);
+            LightSwitch(Lights[5]);
+        }
+        else if(sender == Lights[3])
+        {
+            LightSwitch(Lights[0]);
+            LightSwitch(Lights[1]);
+            LightSwitch(Lights[3]);
+            LightSwitch(Lights[4]);
+            LightSwitch(Lights[6]);
+            LightSwitch(Lights[7]);
+        }
+        else if(sender == Lights[4])
+        {
+            LightSwitch(Lights[0]);
+            LightSwitch(Lights[1]);
+            LightSwitch(Lights[2]);
+            LightSwitch(Lights[3]);
+            LightSwitch(Lights[4]);
+            LightSwitch(Lights[5]);
+            LightSwitch(Lights[6]);
+            LightSwitch(Lights[7]);
+            LightSwitch(Lights[8]);
+            
+        }
+        else if(sender == Lights[5])
+        {
+            LightSwitch(Lights[1]);
+            LightSwitch(Lights[2]);
+            LightSwitch(Lights[4]);
+            LightSwitch(Lights[5]);
+            LightSwitch(Lights[7]);
+            LightSwitch(Lights[8]);
+        }
+        else if(sender == Lights[6])
+        {
+            LightSwitch(Lights[3]);
+            LightSwitch(Lights[4]);
+            LightSwitch(Lights[6]);
+            LightSwitch(Lights[7]);
+        }
+        else if(sender == Lights[7])
+        {
+            LightSwitch(Lights[3]);
+            LightSwitch(Lights[4]);
+            LightSwitch(Lights[5]);
+            LightSwitch(Lights[6]);
+            LightSwitch(Lights[7]);
+            LightSwitch(Lights[8]);
+        }
+        else if(sender == Lights[8])
+        {
+            LightSwitch(Lights[4]);
+            LightSwitch(Lights[5]);
+            LightSwitch(Lights[7]);
+            LightSwitch(Lights[8]);
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +100,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func LightSwitch(button: UIButton) {
+        if(button.backgroundColor == UIColor.whiteColor())
+        {
+            button.backgroundColor = UIColor.blackColor()
+        }
+        else
+        {
+            button.backgroundColor = UIColor.whiteColor()
+        }
+    }
 }
 
